aac2a2057702762e36656f81d693ed4aae86c08a
rajesh <rajeshpgdav3444@gmail.com>
2022-12-01 09:01:29 +0530

    Add project files.

---
+++ RajeshMVC2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RajeshMVC2.Models;
+using RajeshMVC2.Controllers;
+namespace RajeshMVC2.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ApplicationDbContext DbContext { get; }
+
+        public HomeController(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            //string message = "Welcome To ViewBag";
+            //ViewBag.msg = message;
+            //List<string> Students = new List<string> { "Abhay","Akash","Rajesh","Aryan","Hrithik"};
+            //ViewBag.students = Students;
+
+            var employees = DbContext.Employees.ToList();
+            return View(employees);
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
