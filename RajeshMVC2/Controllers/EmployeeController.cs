9b289cdc9a19c2eff3d7fb4c138241a184fb5231
rajesh <rajeshpgdav3444@gmail.com>
2022-12-02 11:26:05 +0530

    Final update up&down

--- RajeshMVC2/Controllers/EmployeeController.cs
+++ RajeshMVC2/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ using RajeshMVC2.Models;
 using RajeshMVC2.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -68,7 +69,14 @@ namespace RajeshMVC2.Controllers
             }
 
             var file = Request.Form.Files["Image"];
-            em.Image = UploadFile(file);
+            string err = string.Empty;
+            em.Image = UploadFile(file,out err);
+            if (!string.IsNullOrEmpty(err))
+            {
+                ViewBag.FileError = err;
+                ViewBag.dept = DbContext.Departments.ToList();
+                return View(employee);
+            }
 
 
             DbContext.Employees.Add(em);
@@ -95,13 +103,61 @@ namespace RajeshMVC2.Controllers
             DbContext.SaveChanges();
             return RedirectToAction("EmployeeList");
         }
-        public string UploadFile(IFormFile formFile)
+        public string UploadFile(IFormFile formFile, out string ErrorMessage)
+        {
+            ErrorMessage = null;
+            string fullpath = string.Empty;
+            if (formFile != null)
+            {
+                string ext = Path.GetExtension(formFile.FileName);
+                if (ext.ToLower() == ".jpg" || ext.ToLower() == ".jpeg" || ext.ToLower() == ".png" || ext.ToLower() == ".gif")
+                {
+                    long fsize = formFile.Length;
+                    if (fsize / 1024 < 100)
+                    {
+
+                        string p = Environment.WebRootPath;
+                        //    string NewFilePath = Guid.NewGuid().ToString() + ext;
+                        string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff",
+                                                CultureInfo.InvariantCulture);
+                        timestamp = timestamp.Replace('-', '_');
+                        timestamp = timestamp.Replace(':', '_');
+                        timestamp = timestamp.Replace('.', '_');
+
+                        string NewFilePath = timestamp + ext;
+                        string path = Path.Combine(p, "Images", NewFilePath);
+                        FileStream fs = new FileStream(path, FileMode.CreateNew);
+                        formFile.CopyTo(fs);
+                       fullpath=Path.Combine("Images", NewFilePath);
+                        return fullpath;
+                    }
+                    else
+                    {
+                        ErrorMessage = "Please select file size max 100kb !!";
+                    }
+                }
+                else
+                {
+                    ErrorMessage = "Please select Image jpf/jpeg file only!!";
+                }
+            }
+            else
+            {
+                ErrorMessage = "Please Upload File!";
+            }
+            return fullpath;
+
+        }
+        
+        public IActionResult DownloadFile(string FileName)
         {
             string p = Environment.WebRootPath;
-            string path = Path.Combine(p, "Images", formFile.FileName);
-            FileStream fs = new FileStream(path,FileMode.CreateNew);
-            formFile.CopyTo(fs);
-            return Path.Combine("Images", formFile.FileName);
+            string fullPath = Path.Combine(p, FileName);
+            string NFileName = FileName.Split("\\")[1];
+            byte[] fileBytes = System.IO.File.ReadAllBytes(fullPath);
+
+            return File(fileBytes,"application/force-download",NFileName);
+
         }
     }
 }
